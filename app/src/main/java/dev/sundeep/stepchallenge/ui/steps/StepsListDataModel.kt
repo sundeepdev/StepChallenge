54d13e74e12675f14d1703660b10b3ed3cf62e95
Sundeep Singh <sundeepdev@gmail.com>
2023-04-09 07:03:05 +0100

    Added Locale to date format

--- app/src/main/java/dev/sundeep/stepchallenge/ui/steps/StepsListDataModel.kt
+++ app/src/main/java/dev/sundeep/stepchallenge/ui/steps/StepsListDataModel.kt
@@ -2,12 +2,13 @@ package dev.sundeep.stepchallenge.ui.steps
 
 import dev.sundeep.stepchallenge.domain.entity.StepData
 import java.text.SimpleDateFormat
+import java.util.*
 
 const val DISPLAY_DATE_FORMAT = "dd/MM/yyyy"
 
 data class StepListUiDataModel(val stepCount: Int, val onDate: String) {
     constructor(stepData: StepData): this(
         stepCount = stepData.count,
-        onDate = SimpleDateFormat(DISPLAY_DATE_FORMAT).format(stepData.date)
+        onDate = SimpleDateFormat(DISPLAY_DATE_FORMAT, Locale.UK).format(stepData.date)
     )
 }
